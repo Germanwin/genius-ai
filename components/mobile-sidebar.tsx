9393aad5e5445c3f598bc5b83260bb3a04a01b18
german <grishingerman7766005@gmail.com>
2024-03-26 13:01:16 +0300

    made a dashboard page and fixed mobile navbar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -8,16 +8,33 @@ import {
     SheetContent
 } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
+import { useState, useEffect } from "react";
 
 export default function MobileSidebar() {
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(
+        () => {
+            setIsMounted(true);
+        }, []
+    );
+    if (!isMounted) {
+        return null;
+    }
+
+
     return (
+        
+
         <Sheet>
             <SheetTrigger>
                 <Button variant='ghost' size='icon' className="md:hidden">
                     <Menu />
                 </Button>
             </SheetTrigger>
-            <SheetContent side="left" className="p-0">
+
+
+
+            <SheetContent side="left" className="p-0 bg-gray-900">
                 <Sidebar />
             </SheetContent>
         </Sheet>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ import MobileSidebar from "@/components/mobile-sidebar";
 export default function Navbar() {
     return (
         <div className="flex items-center p-4">
-            {/* <MobileSidebar /> */}
+            <MobileSidebar />
             <div className="flex w-full justify-end">
                 <UserButton afterSignOutUrl="/" />
             </div>
